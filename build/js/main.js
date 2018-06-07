a2c2c258e2310e39c38d8f9378803b4fcb9bb54d
Grraoo <evil@grraoo.ru>
2018-06-07 16:50:42 +0300

    new sn stats

--- build/js/main.js
+++ build/js/main.js
@@ -272,6 +272,7 @@ var map = {
 const socnets = document.querySelector(`.socnets`);
 
 const buildStatsSn = (periods) => {
+  console.log(periods);
   const prev = [...periods].reverse().find(period => (new Date() >= period.timeStamp));
   const to = periods.find(period => (new Date() < period.timeStamp)) || prev;
   const current = {
@@ -279,6 +280,9 @@ const buildStatsSn = (periods) => {
     to: to,
     get diff() {
       return this.to.total() - this.from.total();
+    },
+    get percent() {
+      return (new Date() - this.from.timeStamp) / ( this.to.timeStamp - this.from.timeStamp);
     },
     get total() {
       const total = {
@@ -288,7 +292,7 @@ const buildStatsSn = (periods) => {
       let max = 0;
       for (const sn in this.to.snets) {
         if (this.to.snets.hasOwnProperty(sn)) {
-          const percent = Math.round(100 * (this.to.snets[sn] - this.from.snets[sn]) / this.diff);
+          const percent = Math.round(100*(this.from.snets[sn] + ((this.to.snets[sn] - this.from.snets[sn]) * this.percent)) / (this.from.total() + this.diff * this.percent ));
           if (percent > max) {
             total.max = sn;
             max = percent;
@@ -305,13 +309,16 @@ const buildStatsSn = (periods) => {
       return total;
     }
   };
+  console.log(current, current.total.snets);
   return current.total.snets;
 };
 
 const printStatsSn = (total) => {
   for (const sn in total) {
-    if (total.hasOwnProperty(sn)) {
-      socnets.querySelector(`#${sn}`).innerText = total[sn];
+    if (sn !== undefined && total.hasOwnProperty(sn)) {
+      console.log(sn, socnets.querySelector(`#${sn}`));
+      if(socnets.querySelector(`#${sn}`)){
+      socnets.querySelector(`#${sn}`).innerText = total[sn];}
     }
   }
   socnets.classList.add(`animated`);
